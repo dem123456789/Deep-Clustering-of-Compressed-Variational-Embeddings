931342688b064f6a13463741bbea5d7f766c785d
dem123456789 <diao_em@hotmail.com>
2019-04-05 16:57:16 -0400

    update test_model

--- src/test_model.py
+++ src/test_model.py
@@ -1,76 +1,82 @@
 import torch
 import config
+config.init()
 import time
 import torch.backends.cudnn as cudnn
 import models
+import os
+import datetime
+import argparse
+import itertools
+from torch import nn
 from data import *
 from utils import *
 from metrics import *
 
 cudnn.benchmark = True
-config.init()
+parser = argparse.ArgumentParser(description='Config')
+for k in config.PARAM:
+    exec('parser.add_argument(\'--{0}\',default=config.PARAM[\'{0}\'], help=\'\')'.format(k))
+args = vars(parser.parse_args())
+for k in config.PARAM:
+    if(config.PARAM[k]!=args[k]):
+        exec('config.PARAM[\'{0}\'] = {1}'.format(k,args[k]))
 for k in config.PARAM:
     exec('{0} = config.PARAM[\'{0}\']'.format(k))
-seeds = list(range(init_seed,init_seed+num_Experiments))
-
-def main():
-    for i in range(num_Experiments):
-        print('Experiment: {}'.format(seeds[i]))
-        runExperiment(seeds[i])
-    return
     
-def runExperiment(seed):
-    print(config.PARAM)
-    resume_model_TAG = '{}_{}_{}'.format(seed,model_data_name,model_name) if(resume_TAG=='') else '{}_{}_{}_{}'.format(seed,model_data_name,model_name,resume_TAG)
-    model_TAG = resume_model_TAG if(special_TAG=='') else '{}_{}'.format(resume_model_TAG,special_TAG)
+def main():
+    seeds = list(range(init_seed,init_seed+num_Experiments))
+    for i in range(num_Experiments):
+        resume_model_TAG = '{}_{}_{}'.format(seeds[i],model_data_name,model_name) if(resume_TAG=='') else '{}_{}_{}_{}'.format(seeds[i],model_data_name,model_name,resume_TAG)
+        model_TAG = resume_model_TAG if(special_TAG=='') else '{}_{}'.format(resume_model_TAG,special_TAG)
+        print('Experiment: {}'.format(model_TAG))
+        result = runExperiment(model_TAG)
+        save(result,'./output/result/{}.pkl'.format(model_TAG))  
+    return
+
+def runExperiment(model_TAG):
+    model_TAG_list = model_TAG.split('_')
+    seed = int(model_TAG_list[0])
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     randomGen = np.random.RandomState(seed)
     
-    train_dataset,test_dataset = fetch_dataset(data_name=test_data_name)
-    valid_data_size = len(train_dataset) if(data_size==0) else data_size
-    _,test_loader = split_dataset(train_dataset,test_dataset,valid_data_size,batch_size=batch_size,radomGen=randomGen)
+    print(config.PARAM)
+    _,test_dataset = fetch_dataset(data_name=test_data_name)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size*world_size, pin_memory=True, num_workers=num_workers*world_size, collate_fn = input_collate)
     best = load('./output/model/{}_best.pkl'.format(resume_model_TAG))
     last_epoch = best['epoch']
-    model = eval('models.{}.{}(test_dataset.classes_size).to(device)'.format(model_dir,model_name))
+    print('Test from {}'.format(last_epoch))
+    model = eval('models.{}.{}(classes_size=test_dataset.classes_size).to(device)'.format(model_dir,model_name)) 
     model.load_state_dict(best['model_dict'])
     test_protocol = init_test_protocol(test_dataset)
-    result = []
-    for i in range(1,num_iter+1):
-        test_meter_panel = test(test_loader,model,last_epoch,test_protocol,i,model_TAG)
-        print_result(last_epoch,i,test_meter_panel)
-        result.append(test_meter_panel)
-    save({'config':config.PARAM,'epoch':last_epoch,'result':result},'./output/result/{}.pkl'.format(model_TAG))  
-    return
-    
-def test(validation_loader,model,epoch,protocol,iter,model_TAG):
-    meter_panel = Meter_Panel(protocol['metric_names'])
+    result = test(test_loader,model,last_epoch,test_protocol)
+    print_result(model_TAG,last_epoch,result) 
+    return result
+            
+def test(validation_loader,model,epoch,protocol):
+    meter_panel = Meter_Panel(metric_names)
     with torch.no_grad():
         model.train(False)
         end = time.time()
         for i, input in enumerate(validation_loader):
             input = collate(input)
             input = dict_to_device(input,device)
-            protocol = update_protocol(input,iter,protocol)  
+            protocol = update_test_protocol(input,protocol)  
             output = model(input,protocol)
             output['loss'] = torch.mean(output['loss']) if(world_size > 1) else output['loss']
             evaluation = meter_panel.eval(input,output,protocol)
             batch_time = time.time() - end
-            meter_panel.update(evaluation,input['img'].size(0))
+            meter_panel.update(evaluation,batch_size)
             meter_panel.update({'batch_time':batch_time})
             end = time.time()
-        save_img(input['img'],'./output/img/image.png')
-        save_img(output['compression']['img'],'./output/img/image_{}_{}_{}.png'.format(model_TAG,epoch,iter))
     return meter_panel
-
+     
 def init_test_protocol(dataset):
     protocol = {}
     protocol['tuning_param'] = config.PARAM['tuning_param'].copy()
-    protocol['metric_names'] = config.PARAM['test_metric_names']
-    protocol['topk'] = config.PARAM['topk']
-    if(config.PARAM['balance']):
-        protocol['classes_counts'] = dataset.classes_counts.expand(world_size,-1).to(device)
-    protocol['loss_mode'] = {'compression':'mae','classification':'ce'}
+    protocol['metric_names'] = config.PARAM['test_metric_names'].copy()
+    protocol['loss_mode'] = config.PARAM['loss_mode']                                                        
     return protocol
     
 def collate(input):
@@ -78,24 +84,23 @@ def collate(input):
         input[k] = torch.stack(input[k],0)
     return input
 
-def update_protocol(input,iter,protocol):
+def update_test_protocol(input,iter,i,num_batch,protocol):
     protocol['num_iter'] = iter
-    protocol['depth'] = config.PARAM['max_depth']
-    protocol['jump_rate'] = config.PARAM['jump_rate']
-    protocol['patch_shape'] = config.PARAM['patch_shape']
-    protocol['img_shape'] = (input['img'].size(2),input['img'].size(3))
-    protocol['step'] = config.PARAM['step']
+    if(i == num_batch-1):
+        protocol['activate_full'] = True
+    else:
+        protocol['activate_full'] = False
     if(input['img'].size(1)==1):
-        protocol['mode'] = 'L'
+        protocol['img_mode'] = 'L'
     elif(input['img'].size(1)==3):
-        protocol['mode'] = 'RGB'
+        protocol['img_mode'] = 'RGB'
     else:
         raise ValueError('Wrong number of channel')
     return protocol
-
-def print_result(epoch,iter,test_meter_panel):
-    print('Test Epoch: {}({}){}'.format(epoch,iter,test_meter_panel.summary(['loss','bpp','psnr','acc'])))
+        
+def print_result(model_TAG,epoch,result):
+    print('Test Epoch({}): {}({}_{}){}'.format(model_TAG,epoch,result.summary(['loss']+config.PARAM['test_metric_names'])))
     return
     
 if __name__ == "__main__":
-    main()
+    main()    
