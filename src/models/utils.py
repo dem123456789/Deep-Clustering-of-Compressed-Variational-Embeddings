fix training issue with gradient clip, add classification, next mc and dc

src/models/utils.py
@@ -73,22 +73,28 @@ def init_param(train_loader, model):
         if config.PARAM['mode'] == 'classification':
             nk = C.sum(dim=0, keepdim=True) + 1e-10
             mu = Z.t().matmul(C) / nk
-            logvar = torch.log((Z ** 2).t().matmul(C) / nk - 2 * mu * Z.t().matmul(C) / nk + mu ** 2)
+            var = (Z ** 2).t().matmul(C) / nk - 2 * mu * Z.t().matmul(C) / nk + mu ** 2
         else:
-            if config.PARAM['init_param_mode'] == 'random':
+            if config.PARAM['init_param_mode'] == 'none':
+                C = torch.distributions.categorical.Categorical(
+                    Z.new_ones(config.PARAM['classes_size']) / config.PARAM['classes_size'])
+                C = idx2onehot(C.sample((Z.size(0),)))
+                mu = model.param['mu'].data
+                var = model.param['var'].data
+            elif config.PARAM['init_param_mode'] == 'random':
                 C = torch.distributions.categorical.Categorical(
                     Z.new_ones(config.PARAM['classes_size']) / config.PARAM['classes_size'])
                 C = idx2onehot(C.sample((Z.size(0),)))
                 nk = C.sum(dim=0, keepdim=True) + 1e-10
                 mu = Z.t().matmul(C) / nk
-                logvar = torch.log((Z ** 2).t().matmul(C) / nk - 2 * mu * Z.t().matmul(C) / nk + mu ** 2)
+                var = (Z ** 2).t().matmul(C) / nk - 2 * mu * Z.t().matmul(C) / nk + mu ** 2
             elif config.PARAM['init_param_mode'] == 'kmeans':
                 from sklearn.cluster import KMeans
                 km = KMeans(n_clusters=config.PARAM['classes_size'], n_init=1).fit(Z.cpu().numpy())
                 C = idx2onehot(torch.tensor(km.labels_, dtype=torch.long))
                 nk = C.sum(dim=0, keepdim=True) + 1e-10
                 mu = Z.t().matmul(C) / nk
-                logvar = torch.log((Z ** 2).t().matmul(C) / nk - 2 * mu * Z.t().matmul(C) / nk + mu ** 2)
+                var = (Z ** 2).t().matmul(C) / nk - 2 * mu * Z.t().matmul(C) / nk + mu ** 2
             elif config.PARAM['init_param_mode'] == 'gmm':
                 from sklearn.mixture import GaussianMixture
                 gm = GaussianMixture(n_components=config.PARAM['classes_size'], covariance_type='diag').fit(
@@ -96,9 +102,9 @@ def init_param(train_loader, model):
                 C = idx2onehot(
                     torch.tensor(gm.predict(Z.cpu().numpy()), dtype=torch.long, device=config.PARAM['device']))
                 mu = torch.tensor(gm.means_.T, dtype=torch.float, device=config.PARAM['device'])
-                logvar = torch.log(torch.tensor(gm.covariances_.T, dtype=torch.float, device=config.PARAM['device']))
+                var = torch.tensor(gm.covariances_.T, dtype=torch.float, device=config.PARAM['device'])
             else:
                 raise ValueError('Not valid init param')
         model.param['mu'].copy_(mu)
-        model.param['logvar'].copy_(logvar)
+        model.param['var'].copy_(var)
         return C

src/models/vade.py
@@ -15,16 +15,15 @@ def reparameterize(mu, logvar):
 
 def loss(input, output, param):
     CE = F.binary_cross_entropy(output['img'], input['img'], reduction='sum')
-    q_c_z = output['label']
+    q_c_z = output['label'] if config.PARAM['mode'] == 'clustering' else idx2onehot(input['label'])
     q_mu = output['mu'].view(input['img'].size(0), -1, 1)
     q_logvar = output['logvar'].view(input['img'].size(0), -1, 1)
     KLD = torch.sum(q_c_z * 0.5 * torch.sum(
-        (q_mu - param['mu']) ** 2 / torch.exp(param['logvar']) + torch.exp(q_logvar) / torch.exp(param['logvar']) - 1 +
-        param['logvar'] - q_logvar, dim=1), dim=1)
-    KLD = KLD + torch.sum(q_c_z * (torch.log(q_c_z) - F.log_softmax(param['logits'], dim=-1)),
-                          dim=1)
+        (q_mu - param['mu']) ** 2 / param['var'] + torch.exp(q_logvar) / param['var'] - 1 + torch.log(
+            param['var']) - q_logvar, dim=1), dim=1)
+    KLD = KLD + torch.sum(q_c_z * (torch.log(q_c_z + 1e-10) - F.log_softmax(param['logits'], dim=-1)), dim=1)
     KLD = KLD.sum()
-    return CE
+    return CE + KLD
 
 
 class VADE(nn.Module):
@@ -32,8 +31,8 @@ class VADE(nn.Module):
         super(VADE, self).__init__()
         self.model = make_model(config.PARAM['model'])
         self.param = nn.ParameterDict({
-            'mu': nn.Parameter(torch.zeros(config.PARAM['latent_size'], config.PARAM['classes_size'])),
-            'logvar': nn.Parameter(torch.ones(config.PARAM['latent_size'], config.PARAM['classes_size'])),
+            'mu': nn.Parameter(torch.randn(config.PARAM['latent_size'], config.PARAM['classes_size'])),
+            'var': nn.Parameter(torch.ones(config.PARAM['latent_size'], config.PARAM['classes_size'])),
             'logits': nn.Parameter(torch.ones(config.PARAM['classes_size']) / config.PARAM['classes_size'])
         })
 
@@ -56,9 +55,8 @@ class VADE(nn.Module):
         else:
             output['z'] = output['mu']
         q_c_z = torch.exp(F.log_softmax(self.param['logits'], dim=-1) - torch.sum(
-            0.5 * (math.log(2 * math.pi) + self.param['logvar']) + (
-                        output['z'].unsqueeze(-1) - self.param['mu']) ** 2 / (2 * torch.exp(self.param['logvar'])),
-            dim=1)) + 1e-10
+            0.5 * torch.log(2 * math.pi * self.param['var']) + (output['z'].unsqueeze(-1) - self.param['mu']) ** 2 / (
+                    2 * self.param['var']), dim=1)) + 1e-10
         output['label'] = q_c_z / torch.sum(q_c_z, dim=1, keepdim=True)
         x = self.model['decoder_latent'](output['z'])
         decoded = self.model['decoder'](x)
@@ -107,4 +105,64 @@ def vade():
          'bias': True, 'normalization': 'none', 'activation': 'sigmoid'})
     config.PARAM['model']['decoder'] = tuple(config.PARAM['model']['decoder'])
     model = VADE()
+    return model
+
+
+def mcvade():
+    normalization = 'none'
+    activation = 'relu'
+    img_shape = config.PARAM['img_shape']
+    latent_size = config.PARAM['latent_size']
+    hidden_size = config.PARAM['hidden_size']
+    num_layers = config.PARAM['num_layers']
+    sharing_rate = config.PARAM['sharing_rate']
+    num_mode = config.PARAM['classes_size']
+    config.PARAM['model'] = {}
+    # Encoder
+    config.PARAM['model']['encoder'] = []
+    config.PARAM['model']['encoder'].append(
+        {'cell': 'LinearCell', 'input_size': np.prod(img_shape).item(), 'output_size': hidden_size,
+         'bias': True, 'num_mode': num_mode, 'normalization': normalization, 'activation': activation})
+    for i in range(num_layers - 2):
+        config.PARAM['model']['encoder'].append(
+            {'cell': 'MultimodalController', 'input_size': hidden_size // (2 ** i), 'num_mode': num_mode,
+             'sharing_rate': sharing_rate})
+        config.PARAM['model']['encoder'].append(
+            {'cell': 'LinearCell', 'input_size': hidden_size // (2 ** i), 'output_size': hidden_size // (2 ** (i + 1)),
+             'bias': True, 'num_mode': num_mode, 'normalization': normalization,
+             'activation': activation})
+    config.PARAM['model']['encoder'] = tuple(config.PARAM['model']['encoder'])
+    # latent
+    config.PARAM['model']['encoder_latent_restriction'] = {
+        'cell': 'MultimodalController', 'input_size': hidden_size // (2 ** (num_layers - 2)), 'num_mode': num_mode,
+        'sharing_rate': sharing_rate}
+    config.PARAM['model']['encoder_latent_mu'] = {
+        'cell': 'LinearCell', 'input_size': hidden_size // (2 ** (num_layers - 2)), 'output_size': latent_size,
+        'bias': True, 'num_mode': num_mode, 'normalization': 'none', 'activation': 'none'}
+    config.PARAM['model']['encoder_latent_logvar'] = {
+        'cell': 'LinearCell', 'input_size': hidden_size // (2 ** (num_layers - 2)), 'output_size': latent_size,
+        'bias': True, 'num_mode': num_mode, 'normalization': 'none', 'activation': 'none'}
+    config.PARAM['model']['decoder_latent_restriction'] = {
+        'cell': 'MultimodalController', 'input_size': latent_size, 'num_mode': num_mode, 'sharing_rate': sharing_rate}
+    config.PARAM['model']['decoder_latent'] = {
+        'cell': 'LinearCell', 'input_size': latent_size, 'output_size': hidden_size // (2 ** (num_layers - 2)),
+        'bias': True, 'num_mode': num_mode, 'normalization': normalization, 'activation': activation}
+    # Decoder
+    config.PARAM['model']['decoder'] = []
+    for i in range(num_layers - 2):
+        config.PARAM['model']['decoder'].append(
+            {'cell': 'MultimodalController', 'input_size': hidden_size // (2 ** (num_layers - 2 - i)), 'num_mode': num_mode,
+             'sharing_rate': sharing_rate})
+        config.PARAM['model']['decoder'].append(
+            {'cell': 'LinearCell', 'input_size': hidden_size // (2 ** (num_layers - 2 - i)),
+             'output_size': hidden_size // (2 ** (num_layers - 2 - i - 1)),
+             'bias': True, 'num_mode': num_mode, 'normalization': normalization, 'activation': activation})
+    config.PARAM['model']['decoder'].append(
+        {'cell': 'MultimodalController', 'input_size': hidden_size, 'num_mode': num_mode,
+         'sharing_rate': sharing_rate})
+    config.PARAM['model']['decoder'].append(
+        {'cell': 'LinearCell', 'input_size': hidden_size, 'output_size': np.prod(img_shape).item(),
+         'bias': True, 'num_mode': num_mode, 'normalization': 'none', 'activation': 'sigmoid'})
+    config.PARAM['model']['decoder'] = tuple(config.PARAM['model']['decoder'])
+    model = MCVADE()
     return model

src/train_model.py
@@ -108,10 +108,11 @@ def runExperiment():
 
 
 def train(data_loader, model, optimizer, logger, epoch):
+    adjust_learning_rate(optimizer, epoch)
+    clip = 0.1
     metric = Metric()
     model.train(True)
     for i, input in enumerate(data_loader):
-        # print(i)
         start_time = time.time()
         input = collate(input)
         input_size = input['img'].numel()
@@ -120,6 +121,7 @@ def train(data_loader, model, optimizer, logger, epoch):
         output = model(input)
         output['loss'] = output['loss'].mean() if config.PARAM['world_size'] > 1 else output['loss']
         output['loss'].backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
         optimizer.step()
         if i % int((len(data_loader) * config.PARAM['log_interval']) + 1) == 0:
             batch_time = time.time() - start_time
@@ -163,7 +165,7 @@ def test(data_loader, model, logger, epoch):
         info = {'info': ['Model: {}'.format(config.PARAM['model_tag']),
                          'Test Epoch: {}({:.0f}%)'.format(epoch, 100.)]}
         logger.append(info, 'test', mean=False)
-        logger.write('test', config.PARAM['metric_names']['test'])
+        logger.write('test', config.PARAM['metric_names']['test'] + ['Clustering Accuracy'])
     return
 
 
@@ -176,14 +178,14 @@ def make_optimizer(model):
                                   weight_decay=config.PARAM['weight_decay'])
     elif config.PARAM['optimizer_name'] == 'Adam':
         optimizer = optim.Adam(model.parameters(), lr=config.PARAM['lr'], weight_decay=config.PARAM['weight_decay'],
-                               betas=(0.5, 0.999))
+                               betas=(0.9, 0.999))
     else:
         raise ValueError('Not valid optimizer name')
     return optimizer
 
 
 def make_scheduler(optimizer):
-    if config.PARAM['scheduler_name'] == 'None':
+    if config.PARAM['scheduler_name'] in ['None', 'Custom']:
         scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[65535])
     elif config.PARAM['scheduler_name'] == 'StepLR':
         scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=config.PARAM['step_size'],
@@ -192,7 +194,7 @@ def make_scheduler(optimizer):
         scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=config.PARAM['milestones'],
                                                    gamma=config.PARAM['factor'])
     elif config.PARAM['scheduler_name'] == 'CosineAnnealingLR':
-        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=config.PARAM['num_epochs'])
+        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=config.PARAM['num_epochs'], eta_min=2e-4)
     elif config.PARAM['scheduler_name'] == 'ReduceLROnPlateau':
         scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=config.PARAM['factor'],
                                                          patience=config.PARAM['patience'], verbose=True,
@@ -204,6 +206,12 @@ def make_scheduler(optimizer):
         raise ValueError('Not valid scheduler name')
     return scheduler
 
+
+def adjust_learning_rate(optimizer, epoch):
+    lr = max(config.PARAM['lr'] * (0.9 ** (epoch // 10)), 2e-4)
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+    return lr
 
 if __name__ == "__main__":
     main()